6cc273f0c27e21887ecd8c2c07196c9f03dba174
MostafaNasser <https://github.com/MoastafaNasser>
2025-01-14 14:37:55 +0200

    create note model

---
+++ lib/models/note_models.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String Title;
+  final String subtitle;
+  final String data;
+  final int color;
+
+  NoteModel({
+    required this.Title,
+    required this.subtitle,
+    required this.data,
+    required this.color,
+  });
+}
